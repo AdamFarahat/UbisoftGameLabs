a2b677e20d5a94749902b99e2f3cfb636e32312c
Adam Farahat <farahatadam@gmail.com>
2026-01-29 13:13:58 -0500

    Add Projectile Prefab
    The player clicks shoot, gamepad button west or spacebar on the keyboard, to instantiate projectiles. No collision is done yet.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,32 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Scripting.APIUpdating;
 
 public class PlayerController : MonoBehaviour
 {
     
-    [SerializeField]float speed = 5.0f;
-    Vector3 direction;
+    [SerializeField] private float speed = 5.0f;
 
-    PlayerInput playerInput;
+    [SerializeField] GameObject projectilePrefab;
+    private Vector3 direction;
+
+    private PlayerInput playerInput;
+
+    private Rigidbody2D rb;
+
+    private Collider2D coll;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Setup Player Inputs
         playerInput = gameObject.GetComponent<PlayerInput>();
         playerInput.actions["Move"].performed += ctx => Move(ctx);
         playerInput.actions["Move"].canceled += ctx => StopMove(ctx);
+        playerInput.actions["Shoot"].performed += _ => Shoot();
+
+        //Setup Rigidbody and Collider
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -40,5 +50,22 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         transform.Translate(direction * speed * Time.fixedDeltaTime);
+    }
+
+    void Shoot()
+    {
+        //Spawns the projectile and sets its direction
+        GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        Projectile projectileScript = projectile.GetComponent<Projectile>();
+        //If no direction, shoot right by default
+        if(direction == Vector3.zero)
+        {
+            projectileScript.SetDirection(Vector2.right);
+        }
+        else
+        {
+            projectileScript.SetDirection(direction);
+        }
+        
     }
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed = 10.0f;
+
+    private Vector2 direction;
+
+    private Rigidbody2D rb;
+
+    private Collider2D coll;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    public void SetDirection(Vector2 dir)
+    {
+        direction = dir.normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(direction * speed * Time.fixedDeltaTime);
+    }
+}
