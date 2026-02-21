4037d55238835cbb6f07a889c3ab8aadd20c4c9b
Diana <edvi.diana@gmail.com>
2026-02-20 23:48:07 -0500

    feat: implemented the healthBar UI

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -7,10 +7,12 @@ public class UIManager : MonoBehaviour
     // [SerializeField] private SwordPlayerController swordPlayerController;
     [SerializeField] private Image gunPlayerCooldownUI;
     [SerializeField] private Image swordPlayerCooldownUI;
+    [SerializeField] private Image healthBarUI;
 
     // temp
     float grenadeCooldown = 1.0f;
     float swordCooldown = 1.0f;
+    float health = 1.0f;
 
     private readonly int amountLeftID = Shader.PropertyToID("_AmountLeft");
     
@@ -21,13 +23,25 @@ public class UIManager : MonoBehaviour
         // Assert.IsNotNull(swordPlayerController);
         Assert.IsNotNull(gunPlayerCooldownUI);
         Assert.IsNotNull(swordPlayerCooldownUI);
+        Assert.IsNotNull(healthBarUI);
+
         Assert.IsNotNull(gunPlayerCooldownUI.material);
         Assert.IsNotNull(swordPlayerCooldownUI.material);
+        Assert.IsNotNull(healthBarUI.material);
+    }
+
+    void Start()
+    {
+        // Create new instances so as not to change the original mats
+        gunPlayerCooldownUI.material = new Material(gunPlayerCooldownUI.material);
+        swordPlayerCooldownUI.material = new Material(swordPlayerCooldownUI.material);
+        healthBarUI.material = new Material(healthBarUI.material);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Update the materials
         grenadeCooldown -= 0.01f; //temp
         // float grenadeCooldown = grenadeBelt.GetCooldownPercentage();
         gunPlayerCooldownUI.material.SetFloat(amountLeftID, grenadeCooldown);
@@ -36,6 +50,10 @@ public class UIManager : MonoBehaviour
         swordCooldown -= 0.01f; //temp
         // float swordCooldown = swordPlayerController.GetCooldownPercentage();
         swordPlayerCooldownUI.material.SetFloat(amountLeftID, swordCooldown);
+
+        health -= 0.005f;// temp
+        // float health = getHealth from player 
+        healthBarUI.material.SetFloat(amountLeftID, health);
         
     }
 }
