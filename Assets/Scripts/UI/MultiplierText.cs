7767f0c7efa2222e9cec5b4bb481475fe20963d6
Diana <edvi.diana@gmail.com>
2026-03-03 13:31:16 -0500

    update: moved multiplier to more readable position and increased overshoot amount

--- Assets/Scripts/UI/MultiplierText.cs
+++ Assets/Scripts/UI/MultiplierText.cs
@@ -4,19 +4,20 @@ using DG.Tweening;
 public class MultiplierText : MonoBehaviour
 {
     [SerializeField] private float animationDuration = 0.5f;
+    [SerializeField] private float overshootAmount = 3f;
 
     public void AnimateTextIn()
     {
         transform.DOKill();
         transform.localScale = Vector3.zero;
-        transform.DOScale(1f, animationDuration).SetEase(Ease.OutBack);
+        transform.DOScale(1f, animationDuration).SetEase(Ease.OutBack, overshootAmount);
     }
 
     public void AnimateTextOut()
     {
         transform.DOKill();
         transform.DOScale(0f, animationDuration)
-            .SetEase(Ease.InBack)
+            .SetEase(Ease.InBack, overshootAmount)
             .OnComplete(DisableObject);
     }
 
