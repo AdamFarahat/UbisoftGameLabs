7f0f5c7cb1e150fda7f26dbdbe305374bd992161
Diana <edvi.diana@gmail.com>
2026-03-04 11:42:06 -0500

    feat: polished button animation logic

--- Assets/Scripts/UI/UIButtonHoverEffect.cs
+++ Assets/Scripts/UI/UIButtonHoverEffect.cs
@@ -4,12 +4,23 @@ using DG.Tweening;
 
 public class UIButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
 {
-    [Header("Hover Settings")]
-    [SerializeField] private float moveAmount = 20f; 
-    [SerializeField] private float speed = 0.2f;
-
     private RectTransform rectTransform;
+    // Hover settings
     private float originalX;
+    private float hoverOffsetX; 
+    private float hoverDuration;
+
+    // Exit settings
+    private bool isExiting = false; 
+    private float exitPositionX; 
+    private float anticipationDuration;
+    private float exitDuration;
+
+    public float HoverOffsetX { get => hoverOffsetX; set => hoverOffsetX = value; }
+    public float HoverDuration { get => hoverDuration; set => hoverDuration = value; }
+    public float ExitPositionX { get => exitPositionX; set => exitPositionX = value; }
+    public float AnticipationDuration { get => anticipationDuration; set => anticipationDuration = value; }
+    public float ExitDuration { get => exitDuration; set => exitDuration = value; }
 
     void Awake()
     {
@@ -18,30 +29,51 @@ public class UIButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointer
     }
 
     // --- MOUSE HOVER ---
-    public void OnPointerEnter(PointerEventData eventData) => MoveOut();
-    public void OnPointerExit(PointerEventData eventData) => MoveBack();
+    public void OnPointerEnter(PointerEventData eventData) => HoverForward();
+    public void OnPointerExit(PointerEventData eventData) => HoverBack();
 
     // --- CONTROLLER/KEYBOARD SELECT ---
-    public void OnSelect(BaseEventData eventData) => MoveOut();
-    public void OnDeselect(BaseEventData eventData) => MoveBack();
+    public void OnSelect(BaseEventData eventData) => HoverForward();
+    public void OnDeselect(BaseEventData eventData) => HoverBack();
 
     // --- ANIMATION LOGIC ---
-    private void MoveOut()
+    private void HoverForward()
     {
+        if (isExiting) return; 
+
         rectTransform.DOKill(); 
-        rectTransform.DOAnchorPosX(originalX + moveAmount, speed).SetEase(Ease.OutQuad);
+        rectTransform.DOAnchorPosX(originalX + hoverOffsetX, hoverDuration).SetEase(Ease.OutQuad);
     }
 
-    private void MoveBack()
+    private void HoverBack()
     {
+        if (isExiting) return; 
+
         rectTransform.DOKill();
-        rectTransform.DOAnchorPosX(originalX, speed).SetEase(Ease.OutQuad);
+        rectTransform.DOAnchorPosX(originalX, hoverDuration).SetEase(Ease.OutQuad);
     }
 
     void OnDisable()
     {
-        // if the menu is turned off while hovered, snap it back to normal
+        // Reset position 
         rectTransform.DOKill();
         rectTransform.anchoredPosition = new Vector2(originalX, rectTransform.anchoredPosition.y);
+        isExiting = false; 
+    }
+
+    public void AnimateOffScreen(float extraPush = 0f)
+    {
+        if (isExiting) return; 
+        isExiting = true; 
+
+        rectTransform.DOKill();
+        Sequence offScreenSeq = DOTween.Sequence();
+
+        // ANTICIPATION: Move forward a bit + any extra push for being selected
+        float totalPush = originalX + hoverOffsetX + extraPush;
+        offScreenSeq.Append(rectTransform.DOAnchorPosX(totalPush, anticipationDuration).SetEase(Ease.OutQuad));
+
+        // EXIT: Move completely off screen.
+        offScreenSeq.Append(rectTransform.DOAnchorPosX(exitPositionX, exitDuration).SetEase(Ease.InQuad));
     }
 }
