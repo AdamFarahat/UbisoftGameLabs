051dff0782af321a8ca293989afb609d8a6e5a72
RyanDeBarros <ryan2004db@gmail.com>
2026-03-05 22:26:57 -0500

    update: extracted bob logic to modular Bobber script

--- Assets/Scripts/Enemy/Flyer/FlyerMovement.cs
+++ Assets/Scripts/Enemy/Flyer/FlyerMovement.cs
@@ -3,19 +3,14 @@ using UnityEngine.Assertions;
 
 public class FlyerMovement : MonoBehaviour
 {
-    [SerializeField] private float bobAmplitude = 0.2f;
-    [SerializeField] private float bobSpeed = 3f;
-    [SerializeField] private Transform bobber;
+    [SerializeField] private float initialLaneDistance = 300f;
     [SerializeField] private float forwardSpeed = 10f;
     [SerializeField] private float damage = 12f;
-
-    private float age = 0f;
 
     private LaneBound lane;
 
     private void Awake()
     {
-        Assert.IsNotNull(bobber);
         lane = GetComponent<LaneBound>();
         Assert.IsNotNull(lane);
         GetComponent<Enemy>().OnTakeFromPool += ResetState;
@@ -23,16 +18,11 @@ public class FlyerMovement : MonoBehaviour
 
     private void ResetState()
     {
-        age = 0f;
+        lane.LaneDistance = initialLaneDistance;
     }
 
     private void Update()
     {
-        age += Time.deltaTime;
-        Vector3 position = bobber.localPosition;
-        position.y = bobAmplitude * Mathf.Sin(bobSpeed * age);
-        bobber.localPosition = position;
-
         lane.LaneDistance -= forwardSpeed * Time.deltaTime;
         
         if (lane.LaneDistance <= 0f)
