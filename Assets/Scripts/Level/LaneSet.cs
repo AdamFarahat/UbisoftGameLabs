42e6d1fe250e1e1140485d59e014713aa049a7cc
RyanDeBarros <ryan2004db@gmail.com>
2026-03-14 18:49:09 -0400

    feat: added lane highlights to better indicate which one the player is on

--- Assets/Scripts/Level/LaneSet.cs
+++ Assets/Scripts/Level/LaneSet.cs
@@ -19,6 +19,14 @@ public class LaneSet : MonoBehaviour
 
     [SerializeField] private float laneSeparation = 12f;
     [SerializeField] private Transform playerTargetHeight;
+    [SerializeField] private SpriteRenderer[] highlights;
+    [SerializeField] private Sprite gunHighlight;
+    [SerializeField] private Sprite swordHighlight;
+    [SerializeField] private Sprite sharedHighlight;
+
+    [SerializeField] private float highlightIndexThreshold = 0.1f;
+    private int gunLaneIndex = -1;
+    private int swordLaneIndex = -1;
 
     private void OnValidate()
     {
@@ -30,6 +38,91 @@ public class LaneSet : MonoBehaviour
         instance = this;
 
         Assert.IsNotNull(playerTargetHeight);
+
+        Assert.IsTrue(highlights.Length == LaneCount);
+        Assert.IsNotNull(gunHighlight);
+        Assert.IsNotNull(swordHighlight);
+        Assert.IsNotNull(sharedHighlight);
+    }
+
+    private void Start()
+    {
+        if (GunPlayerController.Instance != null)
+        {
+            gunLaneIndex = Mathf.FloorToInt(GunPlayerController.LaneIndex);
+            AddGunHighlight();
+        }
+
+        if (SwordPlayerController.Instance != null)
+        {
+            swordLaneIndex = Mathf.FloorToInt(SwordPlayerController.LaneIndex);
+            AddSwordHighlight();
+        }
+    }
+
+    private void Update()
+    {
+        if (GunPlayerController.Instance != null)
+        {
+            int newGunIndex = Mathf.FloorToInt(GunPlayerController.LaneIndex);
+            if (newGunIndex != gunLaneIndex && Mathf.Abs(GunPlayerController.LaneIndex - newGunIndex) < highlightIndexThreshold)
+            {
+                RemoveGunHighlight();
+                gunLaneIndex = newGunIndex;
+                AddGunHighlight();
+            }
+        }
+
+        if (SwordPlayerController.Instance != null)
+        {
+            int newSwordIndex = Mathf.FloorToInt(SwordPlayerController.LaneIndex);
+            if (newSwordIndex != swordLaneIndex && Mathf.Abs(SwordPlayerController.LaneIndex - newSwordIndex) < highlightIndexThreshold)
+            {
+                RemoveSwordHighlight();
+                swordLaneIndex = newSwordIndex;
+                AddSwordHighlight();
+            }
+        }
+    }
+
+    private void AddGunHighlight()
+    {
+        SpriteRenderer highlight = highlights[gunLaneIndex];
+
+        if (highlight.sprite == null)
+            highlight.sprite = gunHighlight;
+        else if (highlight.sprite == swordHighlight)
+            highlight.sprite = sharedHighlight;
+    }
+
+    private void AddSwordHighlight()
+    {
+        SpriteRenderer highlight = highlights[swordLaneIndex];
+
+        if (highlight.sprite == null)
+            highlight.sprite = swordHighlight;
+        else if (highlight.sprite == gunHighlight)
+            highlight.sprite = sharedHighlight;
+    }
+
+    private void RemoveGunHighlight()
+    {
+        SpriteRenderer highlight = highlights[gunLaneIndex];
+
+        if (highlight.sprite == gunHighlight)
+            highlight.sprite = null;
+        else if (highlight.sprite == sharedHighlight)
+            highlight.sprite = swordHighlight;
+    }
+
+    private void RemoveSwordHighlight()
+    {
+        SpriteRenderer highlight = highlights[swordLaneIndex];
+
+        if (highlight.sprite == swordHighlight)
+            highlight.sprite = null;
+        else if (highlight.sprite == sharedHighlight)
+            highlight.sprite = gunHighlight;
     }
 
     public static float PlayerLine
