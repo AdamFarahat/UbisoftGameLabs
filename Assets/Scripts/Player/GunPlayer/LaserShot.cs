0f3b33abaa3c4943699939523ba7b29dc14d84ee
RyanDeBarros <ryan2004db@gmail.com>
2026-03-19 22:19:34 -0400

    feat: added collision and damage logic to laser shot

--- Assets/Scripts/Player/GunPlayer/LaserShot.cs
+++ Assets/Scripts/Player/GunPlayer/LaserShot.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 class LaserShot : MonoBehaviour
 {
     [SerializeField] private float duration = 0.4f;
+    [SerializeField] private int closestDamage = 50;
+    [SerializeField] private float damageRange = 200f;
+    [SerializeField] private float damageFalloffExp = 3f;  // if this is high, farther enemies take more damage
 
     private void Start()
     {
@@ -23,5 +26,25 @@ class LaserShot : MonoBehaviour
 
         gameObject.SetActive(true);
         StartCoroutine(Routine());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Enemy enemy = other.GetComponentInParent<Enemy>();
+        if (enemy == null)
+            return;
+
+        float damage = 0f;
+        float z = enemy.transform.position.z;
+        if (z < LaneSet.PlayerLine)
+            damage = closestDamage;
+        else if (z < damageRange)
+            damage = closestDamage * (1f - Mathf.Pow(z / damageRange, damageFalloffExp));
+
+        if (enemy.TakeDamage(Mathf.FloorToInt(damage)))
+        {
+            GunPlayerController.Instance.AddContinuousMultiplier(GunPlayerController.Instance.GunKillMultiplierGain);
+            GunPlayerController.Instance.AddScore(enemy.Score);
+        }
     }
 }
