13083d302e9fd2b02ef51c2783b1028eed6e90d4
RyanDeBarros <ryan2004db@gmail.com>
2026-03-20 21:43:43 -0400

    feat: GunAnimationManager to manager animations over multiple guns

---
+++ Assets/Scripts/Player/GunPlayer/GunAnimationManager.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class GunAnimationManager : MonoBehaviour
+{
+    [SerializeField] private string[] gunNames;
+    public int gunIndex = 0;
+
+    private SpriteAnimator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<SpriteAnimator>();
+        Assert.IsNotNull(animator);
+
+        Assert.IsTrue(gunNames.Length == 3);
+
+        LaneBound laneBound = GetComponent<LaneBound>();
+        Assert.IsNotNull(laneBound);
+        laneBound.DashStart += OnDashStart;
+        laneBound.DashEnd += OnDashEnd;
+    }
+
+    private void OnDashStart(float deltaLane)
+    {
+        if (deltaLane > 0f)
+            animator.PlayCycle($"{gunNames[gunIndex]} Dash Right");
+        else if (deltaLane < 0f)
+            animator.PlayCycle("Dash Left");
+    }
+
+    private void OnDashEnd()
+    {
+        animator.PlayDefaultCycle();
+    }
+
+    public void PlayIdle()
+    {
+        animator.defaultName = $"{gunNames[gunIndex]} Idle";
+        animator.PlayDefaultCycle();
+    }
+
+    public void PlayShoot()
+    {
+        animator.PlayOneShot($"{gunNames[gunIndex]} Shoot");
+    }
+
+    public void StartGrenadeAim()
+    {
+        animator.PlayCycle("Grenade Aim");
+    }
+
+    public void StopGrenadeAim()
+    {
+        animator.PlayDefaultCycle();
+    }
+
+    public void PlayGrenadeThrow()
+    {
+        animator.PlayOneShot("Grenade Throw");
+    }
+}
