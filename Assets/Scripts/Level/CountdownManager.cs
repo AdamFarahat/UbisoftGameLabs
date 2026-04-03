88bc74b2d75b695e792abf86206110c3d0dc4459
RyanDeBarros <ryan2004db@gmail.com>
2026-04-03 17:12:56 -0400

    feat: countdown in game start

---
+++ Assets/Scripts/Level/CountdownManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+public class CountdownManager : MonoBehaviour
+{
+    // TODO fade out of tutorial
+    [Header("Fade In")]
+    [SerializeField] private RawImage fadeInImage;
+    [SerializeField] private float fadeInDuration = 2f;
+    [SerializeField] private float fadeInExponent = 2f;
+
+    [Header("Countdown")]
+    [SerializeField] private float fadeInDelayFactor = 0.5f;
+    [SerializeField] private Image countdownImage;
+    [SerializeField] private Sprite countdown3;
+    [SerializeField] private Sprite countdown2;
+    [SerializeField] private Sprite countdown1;
+    [SerializeField] private float countdownIndividualDuration = 1f;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(fadeInImage);
+        Assert.IsNotNull(countdownImage);
+        Assert.IsNotNull(countdown3);
+        Assert.IsNotNull(countdown2);
+        Assert.IsNotNull(countdown1);
+    }
+
+    private void Start()
+    {
+        countdownImage.gameObject.SetActive(false);
+        fadeInImage.gameObject.SetActive(true);
+        StartCoroutine(FadeIn());
+        StartCoroutine(Countdown());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        for (float t = 0f; t < fadeInDuration; t += Time.deltaTime)
+        {
+            float a = Mathf.Clamp01(1f - Mathf.Pow(t / fadeInDuration, fadeInExponent));
+            fadeInImage.color = new Color(0f, 0f, 0f, a);
+
+            yield return null;
+        }
+
+        fadeInImage.gameObject.SetActive(false);
+    }
+
+    private IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(fadeInDuration * fadeInDelayFactor);
+
+        countdownImage.gameObject.SetActive(true);
+        countdownImage.sprite = countdown3;
+        yield return new WaitForSeconds(countdownIndividualDuration);
+        countdownImage.sprite = countdown2;
+        yield return new WaitForSeconds(countdownIndividualDuration);
+        countdownImage.sprite = countdown1;
+        yield return new WaitForSeconds(countdownIndividualDuration);
+        countdownImage.gameObject.SetActive(false);
+
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        // TODO level start sfx
+        // TODO start wave spawner
+    }
+}
