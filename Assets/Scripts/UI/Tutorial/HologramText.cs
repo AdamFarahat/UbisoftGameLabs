4c9825f753be3357c2fc321ccb866ef357e40ab2
RyanDeBarros <ryan2004db@gmail.com>
2026-04-06 15:19:01 -0400

    feat: hologram flicker animation

--- Assets/Scripts/UI/Tutorial/HologramText.cs
+++ Assets/Scripts/UI/Tutorial/HologramText.cs
@@ -1,19 +1,57 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class HologramText : MonoBehaviour
 {
+    [SerializeField] private GameObject boxRoot;
+    [SerializeField] private int numberOfFlickers = 2;
+    [SerializeField] private float flickerOnDuration = 0.05f;
+    [SerializeField] private float flickerOffDuration = 0.05f;
+    [SerializeField] private float paddingDuration = 0.1f;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(boxRoot);
+    }
+
     public IEnumerator SpawnRoutine()
     {
-        // TODO flicker on
+        // TODO play new tutorial tip ping SFX
+        
         gameObject.SetActive(true);
-        yield return new WaitForSeconds(0.5f);
+
+        boxRoot.SetActive(false);
+        yield return new WaitForSeconds(paddingDuration);
+
+        for (int i = 0; i < numberOfFlickers; i++)
+        {
+            boxRoot.SetActive(true);
+            yield return new WaitForSeconds(flickerOnDuration);
+            boxRoot.SetActive(false);
+            yield return new WaitForSeconds(flickerOffDuration);
+        }
+
+        boxRoot.SetActive(true);
     }
 
     public IEnumerator DespawnRoutine()
     {
-        // TODO flicker off
+        // TODO play tutorial tip end ping SFX
+
+        boxRoot.SetActive(true);
+
+        for (int i = 0; i < numberOfFlickers; i++)
+        {
+            boxRoot.SetActive(false);
+            yield return new WaitForSeconds(flickerOffDuration);
+            boxRoot.SetActive(true);
+            yield return new WaitForSeconds(flickerOnDuration);
+        }
+
+        boxRoot.SetActive(false);
+        yield return new WaitForSeconds(paddingDuration);
+
         gameObject.SetActive(false);
-        yield return new WaitForSeconds(0.5f);
     }
 }
